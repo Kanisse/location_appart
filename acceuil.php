292b7bbcf5d09c8be99ee3f7d0d4d83146736445
Kanisse <khald.anisse]gmail.com>
2025-06-18 09:06:09 +0100

    background

--- acceuil.php
+++ acceuil.php
@@ -11,7 +11,7 @@ include("db.php");
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 <style>
   body{
-    background:#E0E0E0;
+    background-color:#FFA;
 }
 .details {
             border: 1.5px solid grey;
